fba66c6f0cf7aca909022042bd249189a49559a3
Sarah J Dawns <123122262+sarahJDawns@users.noreply.github.com>
2025-01-13 17:37:41 -0800

    update astro & add project

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
+  type: "content",
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       description: z.string(),
       tags: z.array(z.string()),
       urls: z.object({
-        // site: z.string(),
         github: z.string(),
       }),
       cover: image(),
---
+++ src/content/projects/project-5.md
@@ -0,0 +1,10 @@
+---
+title: HTML Email
+description: Showcasing a collection of HTML email templates demonstrating my skills in crafting visually appealing and functional email designs.
+cover: "../../assets/images/html_email.png"
+coverAlt: image of collection of HTML email templates
+urls: {
+  github: "https://github.com/sarahJDawns/html-email"
+}
+tags: ['HTML', 'CSS']
+---
